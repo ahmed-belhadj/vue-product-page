7151bc65f88a9aeef71b747cd9d179177f9aeeac
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-01-01 11:43:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,32 @@ var app = new Vue({
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: true,
         inventory: 100,
-        onSale: true
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [{
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: 'https://dl.dropboxusercontent.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: 'https://dl.dropboxusercontent.com/s/t32hpz32y7snfna/vmSocks-blue-onWhite.jpg?dl=0'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart -= 1
+        }
     }
 })
